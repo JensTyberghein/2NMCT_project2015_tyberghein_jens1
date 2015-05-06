e0dfc80a9ae0fe4784693fa8fccaf95b7ebed18f
Jens <jens.tyberghein@student.howest.be>
2015-05-06 15:59:03 +0200

    update5

--- app/src/main/java/be/howest/nmct/tankstationlocator/TankstationInfoFragment.java
+++ app/src/main/java/be/howest/nmct/tankstationlocator/TankstationInfoFragment.java
@@ -3,6 +3,7 @@ package be.howest.nmct.tankstationlocator;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,8 +20,13 @@ public class TankstationInfoFragment extends Fragment {
 
     // Data
     List<Tankstation> tankstations;
+    int[] images = {R.drawable.imgtankstation0, R.drawable.imgtankstation1, R.drawable.imgtankstation2, R.drawable.imgtankstation3, R.drawable.imgtankstation4, R.drawable.imgtankstation5, R.drawable.imgtankstation6, R.drawable.imgtankstation7, R.drawable.imgtankstation8, R.drawable.imgtankstation9};
 
     TextView txtInfoNaam;
+    ImageView imgTankstation;
+    TextView txtInfoAdres;
+    TextView txtInfoStad;
+
     Button btnToonOpMap;
 
     // lat en long
@@ -33,6 +40,10 @@ public class TankstationInfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.tankstation_info_fragment,container,false);
 
         txtInfoNaam = (TextView) view.findViewById(R.id.txtInfoNaam);
+        imgTankstation = (ImageView) view.findViewById(R.id.imgTankstation);
+        txtInfoAdres = (TextView) view.findViewById(R.id.txtInfoAdres);
+        txtInfoStad = (TextView) view.findViewById(R.id.txtInfoStad);
+
         btnToonOpMap = (Button) view.findViewById(R.id.btnToonOpMap);
 
         btnToonOpMap.setOnClickListener(new View.OnClickListener() {
@@ -53,11 +64,12 @@ public class TankstationInfoFragment extends Fragment {
     public void changeData(int index){
         tankstations = MyXmlPullParser.getTankstationsFromFile(getActivity());
         txtInfoNaam.setText(tankstations.get(index).getNaam());
+        imgTankstation.setImageResource(images[tankstations.get(index).getId()]);
+        txtInfoAdres.setText(tankstations.get(index).getAdres());
+        txtInfoStad.setText(tankstations.get(index).getStad());
 
         latitude = tankstations.get(index).getLatitude();
         longitude = tankstations.get(index).getLongitude();
-
-
     }
 
     public void btnToonOpMapClicked(){
